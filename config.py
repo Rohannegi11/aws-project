2ec117bd9ff7f0fbeccc98c16c55fe3b211c38fc
Rohan Negi <99790444+Rohannegi11@users.noreply.github.com>
2024-03-03 16:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee01.cloyqya4qspy.ap-south-1.rds.amazonaws.com"
 customuser = "intellipaat"
 custompass = "intellipaat123"
-customdb = "employee01"
+customdb = "employee"
 custombucket = "aws-01project"
 customregion = "ap-south-1"
